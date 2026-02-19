005d61a100f34bc5cf9f012d5e5becd445f82221
Pavel Kalugin <pavel@pavelthebest.me>
2026-02-19 20:02:40 +0300

    hello world server

--- smatri-server/src/main.rs
+++ smatri-server/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+mod util;
+
+use axum::{
+    routing,
+    Router
+};
+
+async fn handler() -> &'static str {
+    "Hello, world!\n"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", routing::get(handler));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
 }
